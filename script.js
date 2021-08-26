11ff8306442457195e646fa3240150f7cebfa874
schny <mdschneider1@gmail.com>
2021-08-26 08:23:21 -0500

    fixed all js functionality for editng cards and formatted css

--- script.js
+++ script.js
@@ -10,6 +10,11 @@
 
 let myLibrary = [];
 let libTable = document.querySelector('#library'); 
+let tdCounter = -1;
+let totalBooks = document.querySelector('.total-books');
+let booksRead = document.querySelector('.books-read');
+let bookCounter = 0;
+let readCounter = 0;
 
 function Book(title, authorLast, authorFirst, pages, read, genre, time, bookID) {
    this.title = title;
@@ -32,15 +37,22 @@ Book.prototype.deleteFromLibrary = function() {
 }
 
 function displayBook(cardID) {
-  item = myLibrary.length - 1;
-
+  let newCard = false;
+  let card = '';
+  let item = '';
   if (cardID == '') {
     card = document.createElement('td');
+    item = myLibrary.length - 1
     card.setAttribute('id', myLibrary[item]['bookID']);
+    newCard = true;
+    
   }
+
   else {
     card = document.getElementById(`${cardID}`);
+    item = getCardIndex(card);
   }
+  
 
   readItems = getReadItems(item);
 
@@ -58,7 +70,20 @@ function displayBook(cardID) {
   <i class="fas fa-edit edit"></i>
   <i class="fa fa-trash delete" aria-hidden="true"></i>`;
 
-  libTable.appendChild(card);
+  if (newCard == true) {
+
+    // add a new row when there are three cards already in the current row
+    if (tdCounter == 3) {
+      tr = document.createElement('tr');
+      libTable.append(tr);
+      tdCounter = 0;
+      
+    }
+    updateBookCounter();
+    updateReadCounter();
+    libTable.appendChild(card);
+  }
+  
 }
 
 addBtn = document.querySelector('#add_book');
@@ -66,7 +91,6 @@ addBtn.addEventListener('click', function() {
   let nextBook = NewBookInfo('', '');
   let newItem = new Book(nextBook.title, nextBook.authorLast, nextBook.authorFirst, nextBook.pages, nextBook.read, nextBook.genre, nextBook.time, nextBook.randBookID);
   newItem.addToLibrary();
-  console.log(myLibrary);
   displayBook('');
 });
 
@@ -76,16 +100,18 @@ addBtn.addEventListener('click', function() {
 
 libTable.onclick = function(event) {
   // change read status button
+  
   changeRead(event);
   deleteCard(event);
   editCard(event);
   
 }
 
+
 const NewBookInfo = (edit, currentID) => {
   let title = document.querySelector(`#${edit}title`).value;
-  let authorLast = document.querySelector(`#${edit}author-last`).value;
-  let authorFirst = document.querySelector(`#${edit}author-first`).value;
+  let authorLast = document.querySelector(`#${edit}authorLast`).value;
+  let authorFirst = document.querySelector(`#${edit}authorFirst`).value;
   let pages = document.querySelector(`#${edit}pages`).value;
   let g = document.querySelector(`#${edit}genre`);
   let genre = g.options[g.selectedIndex].text;
@@ -94,21 +120,13 @@ const NewBookInfo = (edit, currentID) => {
   let currentMonth = currentDate.getMonth();
   let currentYear = currentDate.getFullYear();
   let time = currentDayOfMonth + '/' + (currentMonth + 1) + '/' + currentYear;
- 
-  let read =  document.getElementsByName(`#${edit}read`);
-  for (let i = 0; i < read.length; i++){
-    if (read[i].checked) {
-      read = read[i].value;
-      break;
-    }
-  }
+  let read = $(`input[name="${edit}read"]:checked`).val();
 
-  // figure out how to create new randBookID the first time
-  // but if edited, keep the original book ID
-  
+  // create a random book ID to identify card
   let randBookID = '';
   if (currentID == '') {
      randBookID = Math.random().toString(16).substring(7);
+     tdCounter += 1;
   }
   else {
      randBookID = currentID;
@@ -121,6 +139,7 @@ function editCard(event) {
   let editBtn = event.target.closest('.edit');
   let currentCard = event.target.closest('td');
   
+  
   if (!editBtn) return;
   
   //add functionality to button and modal window close button
@@ -130,30 +149,43 @@ function editCard(event) {
   function toggleModal () {
     editWindow.classList.toggle('show-modal');
   }
-  
+
+  cardIndex = getCardIndex(currentCard);
+  console.log(cardIndex);
+
+  // get new values of card to populate input fields of edit_ elements
+  libLoopArr = ['title','authorLast','authorFirst','pages'];
+  for (let i = 0; i < libLoopArr.length; i++) {
+    console.log(`edit_${libLoopArr[i]}`);
+    console.log(myLibrary[cardIndex][libLoopArr[i]]);
+    document.getElementById(`edit_${libLoopArr[i]}`).value = myLibrary[cardIndex][libLoopArr[i]];
+  }
+  let edit_read = myLibrary[cardIndex]['read'];
+  let edit_genre = myLibrary[cardIndex]['genre'].toLowerCase();
+
+  $(`input[name='edit_read'][value='${edit_read}']`).prop("checked",true);
+  $('#edit_genre').val(`${edit_genre}`);
+
   toggleModal();
 
   closeWindow.onclick = function() {
     toggleModal();
   }
 
+  cardID = currentCard.getAttribute('id');
+
   // get edited entry info and make edit to card
   editWindow.onclick = function(event) {
-    editEntry(event, currentCard, editWindow);
+    editEntry(event, cardID, cardIndex, editWindow);
   }
 
 }
 
-
-function editEntry(event, card, editWindow) {
+function editEntry(event, cardID, cardIndex, editWindow) {
   editEntryBtn = event.target.closest('.edit-entry');
   if (!editEntryBtn) return;
 
-  cardID = card.getAttribute('id');
-  cardIndex = getCardIndex(card);
-
   let editedBook = NewBookInfo('edit_', cardID);
-  console.log(cardIndex);
 
   // edit entry in myLibrary
   myLibrary[cardIndex]['title'] = editedBook.title;
@@ -165,6 +197,7 @@ function editEntry(event, card, editWindow) {
   
   // edit card with new book info and close window
   displayBook(cardID);
+  updateReadCounter();
   editWindow.classList.toggle('show-modal')
 
 }
@@ -175,11 +208,12 @@ function deleteCard(event) {
   let currentCard = event.target.closest('td');
   if (!delBtn) return;
 
+  updateReadCounter();
+  updateBookCounter();
   cardID = currentCard.getAttribute('id');
   myCard = getCardIndex(currentCard);
   myLibrary.splice(myCard, 1);
   libTable.removeChild(currentCard);
-  console.log(myLibrary);
 }
 
 
@@ -199,21 +233,23 @@ function changeRead(event) {
     changeBtn.classList.remove('not_read');
   }
 
+  
   readItems = getReadItems(myCard);
+  
+  // update readCounter
+  updateReadCounter();
+
   changeBtn.classList.add(`${readItems.readClass}`);
   changeBtn.innerHTML = `${readItems.readIcon}<br>${readItems.readName}`;
 }
 
 function getCardIndex(card) {
-  let myCard = 0;
   cardID = card.getAttribute('id');
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i]['bookID'] == cardID){
-      myCard = i;
-      break;
+      return i;
     }
   }
-  return myCard;
 }
 
 const getReadItems = (item) => {
@@ -233,18 +269,46 @@ const getReadItems = (item) => {
   return {readIcon, readClass, readName};
 }
 
-// allow user to drag and drop cards
-$( function() {
-  $( ".sortable" ).sortable({
-    revert: true
-  });
-  $( ".draggable" ).draggable({
-    connectToSortable: ".sortable",
-    helper: "clone",
-    revert: "invalid"
-  });
-  $( 'td' ).disableSelection();
-} );
+// allow user to drag and drop cards (functionality removed)
+// $( function() {
+//   $( ".sortable" ).sortable({
+//     revert: true
+//   });
+//   $( ".draggable" ).draggable({
+//     connectToSortable: ".sortable",
+//     helper: "clone",
+//     revert: "invalid"
+//   });
+//   $( 'td' ).disableSelection();
+// } );
+
+
+// get numbers for total books and total books read and display
+
+
+
+
+const getBooksRead = () => {
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i]['read'] == 'Read') {
+      readCounter += 1;
+      console.log(readCounter);
+    }
+  }
+  return readCounter;
+}
+
+function updateReadCounter() {
+  readCounter = 0;
+  totalBooksRead = getBooksRead();
+  booksRead.innerHTML = `Total read: ${totalBooksRead}`;
+}
+
+function updateBookCounter() {
+  bookCounter = myLibrary.length;
+  console.log(myLibrary);
+  totalBooks.innerHTML = `Total books: ${bookCounter}`;
+}
 
 // get user name for personalized reading list
 $(document).ready(function(){
@@ -257,3 +321,4 @@ $(document).ready(function(){
     userLib.innerHTML = `${userName}'s reading list`;
   }
 });
+
